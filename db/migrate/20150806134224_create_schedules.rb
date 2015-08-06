de1532793aa199d219c57304a15b7e53600d01d4
Derek Jacobi <derekjacobi@gmail.com>
2015-08-06 11:58:06 -0400

    teams

--- db/migrate/20150806134224_create_schedules.rb
+++ db/migrate/20150806134224_create_schedules.rb
@@ -1,9 +1,3 @@
 class CreateSchedules < ActiveRecord::Migration
-  def change
-    create_table :schedules do |t|
-      t.column :nfl, :json
-      t.column :nba, :json
-      t.column :mlb, :json   
-    end
-  end
+
 end
