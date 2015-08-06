d618145fbc352ca0adb1095743ec17a6d2174018
Derek Jacobi <derekjacobi@gmail.com>
2015-08-06 09:55:10 -0400

    sports teams variables

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,16 @@
 class GamesController < ApplicationController
+  before_action :set_team
 
   def index
+  end
+
+  def show
+  end
+
+  private
+
+  def set_team
+    @team = params[:team].capitalize
   end
 
 end
