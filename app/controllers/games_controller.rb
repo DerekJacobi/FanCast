86005a59270ba6a720ab4ee6ab41c2a971fa066c
Derek Jacobi <derekjacobi@gmail.com>
2015-08-07 08:49:36 -0400

    team pages

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -26,7 +26,7 @@ class GamesController < ApplicationController
       @capacity = en3[0]["venue"]["capacity"]
       @surface = en3[0]["venue"]["surface"]
       @type = en3[0]["venue"]["type"]
-      
+      fail
   end
 
 
