6db05f5c6acfb2d531fe9173f064e11e3c17f941
Derek Jacobi <derekjacobi@gmail.com>
2015-08-09 21:44:03 -0400

    create a broadcast

--- app/controllers/broadcasts_controller.rb
+++ app/controllers/broadcasts_controller.rb
@@ -19,47 +19,51 @@ class BroadcastsController < ApplicationController
 
 
   def new
-    @broadcaster = Broadcast.new
+    @broadcast = Broadcast.new
   end
 
   def edit
     @broadcaster_back = broadcaster.find_by(id:current_user.id)
   end
 
+  def makebroadcast
+    @nfl = Schedule.first.content['weeks'][0]['games']
+    @current_user
+  end
 
-  # def create
-  #   @broadcaster = Broadcast.new(broadcaster_params)
+  def create
+    @broadcast = Broadcast.new(broadcast_params)
+    respond_to do |format|
+      if @broadcast.save
+        format.html { redirect_to @broadcast, notice: 'broadcaster was successfully created.' }
+        format.json { render :show, status: :created, location: @broadcast }
+      else
+        format.html { render :new }
+        format.json { render json: @broadcast.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  # def update
   #   respond_to do |format|
-  #     if @broadcaster.save
-  #       format.html { redirect_to @broadcaster, notice: 'broadcaster was successfully created.' }
-  #       format.json { render :show, status: :created, location: @broadcaster }
+  #     if @broadcaster.update(user_params)
+  #       format.html { redirect_to @broadcaster, notice: 'broadcaster was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @broadcaster }
   #     else
-  #       format.html { render :new }
+  #       format.html { render :edit }
   #       format.json { render json: @broadcaster.errors, status: :unprocessable_entity }
   #     end
   #   end
-  #
   # end
-
-  def update
-    respond_to do |format|
-      if @broadcaster.update(user_params)
-        format.html { redirect_to @broadcaster, notice: 'broadcaster was successfully updated.' }
-        format.json { render :show, status: :ok, location: @broadcaster }
-      else
-        format.html { render :edit }
-        format.json { render json: @broadcaster.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @broadcaster.destroy
-    respond_to do |format|
-      format.html { redirect_to broadcasts_url, notice: 'broadcaster was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #
+  # def destroy
+  #   @broadcaster.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to broadcasts_url, notice: 'broadcaster was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
@@ -72,7 +76,8 @@ class BroadcastsController < ApplicationController
       end
     end
 
-    def broadcaster_params
-      params.require(:broadcast).permit(:game_id, :user_id)
+    def broadcast_params
+      params.permit(:game_id, :user_id)
     end
+
 end
